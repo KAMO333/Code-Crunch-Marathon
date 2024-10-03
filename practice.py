1bd31cf48fc30bbc3a8d35644c7fd21c08917cd8
Kamogelo Mmopane <mmopanemoroka@gmail.com>
2024-10-03 10:30:43 +0200

    get dict values

--- practice.py
+++ practice.py
@@ -23,3 +23,15 @@ list_nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 student_names = ["jason", "james", "jane", "jill", "jerry"]
 
+def pass_students():
+
+    for key, value in students.items():
+        if value > 50:
+            print(key + ' ' + 'passed')
+
+    
+
+pass_students()
+
+
+
