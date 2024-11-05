3d1c1ed043a4b8e83bdce7d223476e79d7622806
Kamogelo Mmopane <101883751+KAMO333@users.noreply.github.com>
2024-11-05 20:55:31 +0200

    logic to find the thief

--- SQL/find_partner/trace_transaction.sql
+++ SQL/find_partner/trace_transaction.sql
@@ -1,4 +1,7 @@
-SELECT * 
+DROP TABLE IF EXISTS atm_withdrawals;
+
+CREATE TEMPORARY TABLE atm_withdrawals AS
+SELECT *
 FROM atm_transactions
 WHERE day = 28 
 AND month = 7 
@@ -6,3 +9,9 @@ AND year = 2023
 AND atm_location = 'Leggett Street'
 And transaction_type = 'withdraw' 
 LIMIT 100
+
+
+SELECT * FROM atm_withdrawals;
+
+-- all withdrawal transactions made at Leggett Street on the 28th of July 2023
+
--- SQL/find_thief/info_from_witnessess.sql
+++ SQL/find_thief/info_from_witnessess.sql
@@ -6,8 +6,8 @@ AND year = 2023
 
 -- from witness statement I get: 
 
--- 1. Call less than 60 sec. 
+-- 1. Call less than 60 sec. -- done
 -- 2. flight the following morning. 
 -- 3. Flight tickets.
--- 4. cars leaving parking lot around the same time. 
+-- 4. cars leaving parking lot around the same time. -- done 
 -- 5. ATM transactions on Legget street.
