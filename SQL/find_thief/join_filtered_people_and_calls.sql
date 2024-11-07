76538bf19e2625b6c06b24885b64558c2413bc2c
Kamogelo Mmopane <101883751+KAMO333@users.noreply.github.com>
2024-11-07 13:05:45 +0200

    dropped my suspects list to two

--- SQL/find_thief/join_filtered_people_and_calls.sql
+++ SQL/find_thief/join_filtered_people_and_calls.sql
@@ -1,7 +1,13 @@
+DROP TABLE IF EXISTS suspects;
+
+CREATE TEMPORARY TABLE suspects AS
 SELECT *
 FROM temp_filtered_phone_calls pc
 JOIN temp_filtered_people fp 
   ON pc.caller = fp.phone_number;
+
+
+SELECT * FROM suspects
 
 -- Join the temp files from calls and people into one file of possible suspects.
 -- with their names, numbers and number plates.
---
+++ SQL/using_banking_details.sql/use_account_numbers.sql
@@ -0,0 +1,30 @@
+-- Drop the temporary table if it exists
+DROP TABLE IF EXISTS persons_account_numbers;
+
+-- Create a temporary table that holds account numbers from atm_withdrawals
+CREATE TEMPORARY TABLE persons_account_numbers AS
+SELECT DISTINCT account_number
+FROM atm_withdrawals;
+
+-- Select data from the temporary table
+SELECT * FROM persons_account_numbers;
+
+-- Use account_number to find the person ID from the bank_account table
+SELECT ba.person_id, pan.account_number
+FROM persons_account_numbers pan
+JOIN bank_accounts ba
+ON pan.account_number = ba.account_number;
+
+--used ID from bank account to find names of people who made withdrawals 
+-- and look for person who appear in both persons_account_numbers and suspects temporary table
+SELECT p.name, p.phone_number, pan.account_number
+FROM persons_account_numbers pan
+JOIN bank_accounts ba ON pan.account_number = ba.account_number
+JOIN people p ON ba.person_id = p.id
+JOIN suspects s ON p.name = s.name;
+
+
+
+
+
+
